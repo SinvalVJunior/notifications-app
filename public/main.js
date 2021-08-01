54162d2ee7e19ac99c88f9be96399ef78ba09d83
Sinval <sinvalvieirajunior@gmail.com>
2021-07-31 23:53:34 -0300

    Removing unecessary steps

--- public/main.js
+++ public/main.js
@@ -38,10 +38,7 @@ function subscribeUserToPush() {
   return navigator.serviceWorker.register('sw.js')
     .then(function (registration) {
       const subscribeOptions = {
-        userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(
-          'BETZY4598TinToe6XDeERjLgEnnu-iur3upWomSWfjDCfzQEExt8YgwDZuiV61qa-dvarW0o5I0_HRYXAsTS_qg'
-        )
+        userVisibleOnly: true
       };
 
       return registration.pushManager.subscribe(subscribeOptions);
@@ -51,20 +48,3 @@ function subscribeUserToPush() {
       return pushSubscription;
     });
 }
-
-function urlBase64ToUint8Array(base64String) {
-  var padding = '='.repeat((4 - base64String.length % 4) % 4);
-  var base64 = (base64String + padding)
-      .replace(/\-/g, '+')
-      .replace(/_/g, '/');
-
-  var rawData = window.atob(base64);
-  var outputArray = new Uint8Array(rawData.length);
-
-  for (var i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
-
