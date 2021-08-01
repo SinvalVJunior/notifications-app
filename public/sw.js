440d70b532861d590a5f999caddb15035160f26e
Sinval <sinvalvieirajunior@gmail.com>
2021-07-31 23:15:49 -0300

    Adding subscribe function to push manager

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,23 @@
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js').then(function(reg) {
-      console.log('Service Worker Registered!', reg);
-      reg.pushManager.getSubscription().then(function(sub) {
-        if (sub === null) {
-          // Update UI to ask user to register for Push
-          console.log('Not subscribed to push service!');
-        } else {
-          // We have a subscription, update the database
-          console.log('Subscription object: ', sub);
-        }
-      });
-    })
-     .catch(function(err) {
-      console.log('Service Worker registration failed: ', err);
-    });
+function subscribeUserToPush() {
+  return navigator.serviceWorker.register('sw.js')
+  .then(function(registration) {
+    const subscribeOptions = {
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(
+        'BETZY4598TinToe6XDeERjLgEnnu-iur3upWomSWfjDCfzQEExt8YgwDZuiV61qa-dvarW0o5I0_HRYXAsTS_qg'
+      )
+    };
+
+    return registration.pushManager.subscribe(subscribeOptions);
+  })
+  .then(function(pushSubscription) {
+    console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+    return pushSubscription;
+  });
 }
+
+subscribeUserToPush();
 
 self.addEventListener('push', function(e) {
     var options = {
